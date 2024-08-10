2e7605aa4ff0201a55c6167e82e977d967ddc2c9
Andrew DeFord <sixdefords50@gmail.com>
2024-08-10 18:04:37 -0400

    Added precompiled header

---
+++ RealEngine/src/repch.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "RealEngine/Core/Log.h"
+#include "RealEngine/Core/Assert.h"
+#include "RealEngine/Core/Profiler.h"
+
+#include "RealEngine/Core/Core.h"
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <optional>
+#include <algorithm>
+#include <functional>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+
+#include <thread>
+#include <mutex>
+
+#include <algorithm>
+#include <sstream>
+#include <array>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+#ifdef RE_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif 
