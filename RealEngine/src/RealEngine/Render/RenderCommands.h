6b47d3ed299eabb6a2ce17268577829d89ba1886
Andthehand <sixdefords50@gmail.com>
2025-02-26 16:26:13 -0500

    Query GPU for make and version on startup

--- RealEngine/src/RealEngine/Render/RenderCommands.h
+++ RealEngine/src/RealEngine/Render/RenderCommands.h
@@ -5,6 +5,22 @@
 namespace RealEngine {
 	class RenderCommands {
 	public:
+		static void Init() {
+			//glEnable(GL_BLEND);
+			//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			//glEnable(GL_DEPTH_TEST);
+
+			const char* vendor = (char*)glGetString(GL_VENDOR);
+			const char* renderer = (char*)glGetString(GL_RENDERER);
+			const char* version = (char*)glGetString(GL_VERSION);
+			const char* glslVersion = (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+			RE_CORE_INFO("OpenGL Info:");
+			RE_CORE_INFO("    OpenGL Vendor: {0}", vendor);
+			RE_CORE_INFO("    OpenGL Renderer: {0}", renderer);
+			RE_CORE_INFO("    OpenGL Version: {0}", version);
+			RE_CORE_INFO("    GLSL Version: {0}", glslVersion);
+		}
+
 		static void Clear() {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		}
