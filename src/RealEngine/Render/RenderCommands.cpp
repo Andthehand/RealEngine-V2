579f322a797fd7b9c83c188557986d193551d9f0
Andthehand <sixdefords50@gmail.com>
2025-03-30 16:37:30 -0400

    Added Opengl Error callback

---
+++ src/RealEngine/Render/RenderCommands.cpp
@@ -0,0 +1,94 @@
+#include "RenderCommands.h"
+
+namespace RealEngine {
+	void OpenGLMessageCallback(
+		unsigned source,
+		unsigned type,
+		unsigned id,
+		unsigned severity,
+		int length,
+		const char* message,
+		const void* userParam) {
+		RE_PROFILE_FUNCTION();
+
+		switch (severity) {
+			case GL_DEBUG_SEVERITY_HIGH:			RE_CORE_CRITICAL("GLFW Error: {}", message); return;
+			case GL_DEBUG_SEVERITY_MEDIUM:			RE_CORE_ERROR("GLFW Error: {}", message); return;
+			case GL_DEBUG_SEVERITY_LOW:				RE_CORE_WARN("GLFW Error: {}", message); return;
+			case GL_DEBUG_SEVERITY_NOTIFICATION:	RE_CORE_TRACE("GLFW Error: {}", message); return;
+		}
+
+		RE_CORE_ASSERT(false, "Unknown severity level!");
+	}
+
+	void RenderCommands::Init() {
+		RE_PROFILE_FUNCTION();
+		//glEnable(GL_BLEND);
+		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+#ifdef RE_DEBUG
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_FALSE);
+#endif
+
+		const char* vendor = (char*)glGetString(GL_VENDOR);
+		const char* renderer = (char*)glGetString(GL_RENDERER);
+		const char* version = (char*)glGetString(GL_VERSION);
+		const char* glslVersion = (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+		RE_CORE_INFO("OpenGL Info:");
+		RE_CORE_INFO("    OpenGL Vendor: {0}", vendor);
+		RE_CORE_INFO("    OpenGL Renderer: {0}", renderer);
+		RE_CORE_INFO("    OpenGL Version: {0}", version);
+		RE_CORE_INFO("    GLSL Version: {0}", glslVersion);
+
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_MULTISAMPLE);
+	}
+
+	void RenderCommands::Clear() {
+		RE_PROFILE_FUNCTION();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void RenderCommands::SetClearColor(const glm::vec4& color) {
+		glClearColor(color.r, color.g, color.b, color.a);
+	}
+
+	void RenderCommands::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
+		glViewport(x, y, width, height);
+	}
+
+	void RenderCommands::SetWireframe(bool enabled) {
+		glPolygonMode(GL_FRONT_AND_BACK, enabled ? GL_LINE : GL_FILL);
+	}
+
+	void RenderCommands::SetFaceCulling(bool enabled) {
+		if (enabled) {
+			glEnable(GL_CULL_FACE);
+			glCullFace(GL_BACK);
+		}
+		else {
+			glDisable(GL_CULL_FACE);
+		}
+	}
+
+	void RenderCommands::DrawIndexed(Ref<VertexArray> vertexArray, uint32_t indexCount) {
+		RE_PROFILE_FUNCTION();
+		vertexArray->Bind();
+
+		uint32_t count = indexCount == 0 ? vertexArray->GetIndexBuffer()->GetCount() : indexCount;
+		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
+	}
+
+	void RenderCommands::DrawArrays(Ref<VertexArray> vertexArray, uint32_t count) {
+		RE_PROFILE_FUNCTION();
+		vertexArray->Bind();
+
+		glDrawArrays(GL_TRIANGLES, 0, count);
+	}
+
+}
--- src/RealEngine/Render/RenderCommands.h
+++ src/RealEngine/Render/RenderCommands.h
@@ -7,66 +7,18 @@
 namespace RealEngine {
 	class RenderCommands {
 	public:
-		static inline void Init() {
-			RE_PROFILE_FUNCTION();
-			//glEnable(GL_BLEND);
-			//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-			glEnable(GL_DEPTH_TEST);
-			glEnable(GL_MULTISAMPLE);
+		static void Init();
 
-			const char* vendor = (char*)glGetString(GL_VENDOR);
-			const char* renderer = (char*)glGetString(GL_RENDERER);
-			const char* version = (char*)glGetString(GL_VERSION);
-			const char* glslVersion = (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
-			RE_CORE_INFO("OpenGL Info:");
-			RE_CORE_INFO("    OpenGL Vendor: {0}", vendor);
-			RE_CORE_INFO("    OpenGL Renderer: {0}", renderer);
-			RE_CORE_INFO("    OpenGL Version: {0}", version);
-			RE_CORE_INFO("    GLSL Version: {0}", glslVersion);
-		}
+		static void Clear();
+		static void SetClearColor(const glm::vec4& color);
 
-		static inline void Clear() {
-			RE_PROFILE_FUNCTION();
+		static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
+		static void SetWireframe(bool enabled);
 
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		}
+		static void SetFaceCulling(bool enabled);
 
-		static inline void SetClearColor(const glm::vec4& color) {
-			glClearColor(color.r, color.g, color.b, color.a);
-		}
-
-		static inline void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
-			glViewport(x, y, width, height);
-		}
-
-		static inline void SetWireframe(bool enabled) {
-			glPolygonMode(GL_FRONT_AND_BACK, enabled ? GL_LINE : GL_FILL);
-		}
-
-		static inline void SetFaceCulling(bool enabled) {
-			if (enabled) {
-				glEnable(GL_CULL_FACE);
-				glCullFace(GL_BACK);
-			}
-			else {
-				glDisable(GL_CULL_FACE);
-			}
-		}
-
-		static inline void DrawIndexed(Ref<VertexArray> vertexArray, uint32_t indexCount = 0) {
-			RE_PROFILE_FUNCTION();
-			vertexArray->Bind();
-
-			uint32_t count = indexCount == 0 ? vertexArray->GetIndexBuffer()->GetCount() : indexCount;
-			glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
-		}
-
+		static void DrawIndexed(Ref<VertexArray> vertexArray, uint32_t indexCount = 0);
 		//TODO: If you pass in 0 for count, it will draw the entire buffer by querying the buffer size from the vertexArray
-		static inline void DrawArrays(Ref<VertexArray> vertexArray, uint32_t count) {
-			RE_PROFILE_FUNCTION();
-			vertexArray->Bind();
-
-			glDrawArrays(GL_TRIANGLES, 0, count);
-		}
+		static void DrawArrays(Ref<VertexArray> vertexArray, uint32_t count);
 	};
 }
