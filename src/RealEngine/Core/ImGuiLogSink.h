f11b22a6356868fef61c8fcb8f37094b216292f9
Andthehand <sixdefords50@gmail.com>
2025-10-18 01:19:07 -0400

    Changed ImGui logger info

--- src/RealEngine/Core/ImGuiLogSink.h
+++ src/RealEngine/Core/ImGuiLogSink.h
@@ -23,24 +23,24 @@ namespace RealEngine {
             std::string_view /*thread_id*/,
             std::string_view /*thread_name*/, 
             std::string const& /*process_id*/, 
-            std::string_view /*logger_name*/,
+            std::string_view logger_name,
             quill::LogLevel log_level,
             std::string_view /*log_level_description*/, 
             std::string_view /*log_level_short_code*/,
             std::vector<std::pair<std::string, std::string>> const* /*named_args*/,
-            std::string_view /*log_message*/, 
-            std::string_view log_statement) noexcept override {
+            std::string_view log_message, 
+            std::string_view /*log_statement*/) noexcept override {
             // log_statement contains the already-formatted message (pattern applied).
             // We copy to a std::string and push into the queue for the UI thread.
             // Keep write_log very small and noexcept to avoid destabilizing the backend.
-            g_Queue.Push(ImGuiLog{ std::string(log_statement), log_level });
+            g_Queue.Push(ImGuiLog{ std::string(std::string(logger_name) + ": " + std::string(log_message)), log_level });
         }
 
         void flush_sink() override {
             // No-op
 		}
 
-		static const RingBuffer<1024, ImGuiLog>& GetQueue() { return g_Queue; }
+		static RingBuffer<1024, ImGuiLog>& GetQueue() { return g_Queue; }
     private:
 		static inline RingBuffer<1024, ImGuiLog> g_Queue;
 	};
